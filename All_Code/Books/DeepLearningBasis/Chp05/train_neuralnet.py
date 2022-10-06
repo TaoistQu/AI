8b12b5c047fc2f568eb6a716520b34158216b846
TaoistQu <qulei_20180331@163.com>
2022-10-07 00:59:43 +0800

    实现梯度优化器

--- All_Code/Books/DeepLearningBasis/Chp05/train_neuralnet.py
+++ All_Code/Books/DeepLearningBasis/Chp05/train_neuralnet.py
@@ -13,7 +13,6 @@ import numpy as np
 
 (x_train,t_train),(x_test,t_test) = load_mnist(normalize=True,one_hot_label=True)
 network = TwoLayerNet(input_size=784,hidden_size=50,output_size=10)
-
 iters_num = 10000
 train_size = x_train.shape[0]
 batch_size = 100
