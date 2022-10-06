5953f859d69109cfce67e176adf84034906f4acc
TaoistQu <qulei_20180331@163.com>
2022-10-06 15:36:18 +0800

    用计算图实现简单的单个乘法的向前和向后传播实现

---
+++ All_Code/Books/DeepLearningBasis/Chp05/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2022 #
+# @Time    : 2022/10/5 13:05
+# @Author  : TaoistQu
+# @Email   : qulei_20180331@163.com
+# @File    : __init__.py.py
+# @Software: PyCharm
---
+++ All_Code/Books/DeepLearningBasis/Chp05/layer_naive.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2022 #
+# @Time    : 2022/10/6 15:20
+# @Author  : TaoistQu
+# @Email   : qulei_20180331@163.com
+# @File    : layer_naive.py
+# @Software: PyCharm
+
+
+class MulLayer:
+    def __init__(self):
+        self.x = None
+        self.y = None
+
+
+    def forward(self,x,y):
+        self.x  = x
+        self.y = y
+        out = x*y
+        return out
+
+    def backward(self,dout):
+        dx = dout*self.y
+        dy = dout*self.x
+        return dx,dy
+
+    class AddLayer:
+        def __init__(self):
+            pass
+        def forward(self,x,y):
+            out = x+y
+            return out
+
+        def backward(self,dout):
+            dx = dout*1
+            dy = dout*1
+
+            return dx,dy
