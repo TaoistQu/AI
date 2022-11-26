6758b781103664bd4bd573e20d880189101d04d5
TaoistQu <qulei_20180331@163.com>
2022-11-26 21:35:13 +0800

    knn模型保存和加载

--- Base/knn/knn.py
+++ Base/knn/knn.py
@@ -11,14 +11,23 @@ from IPython.core.display import display
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
+import joblib
 
 X,y = datasets.load_iris(return_X_y=True)# 鸢尾花，分三类，鸢尾花花萼和花瓣长宽
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2)
 
 knn = KNeighborsClassifier(n_neighbors=5)
 knn.fit(X_train,y_train)
-y_pre = knn.predict(X_test)
+y_ = knn.predict(X_test)
 acc = (y_test == knn.predict(X_test)).mean()
 print(knn.score(X_test,y_test))
 display(acc)
+#模型保存
+joblib.dump(knn,'./model')
+model = joblib.load('./model')
+print(type(model))
+y_ = model.predict(X_test)
+display(y_)
+score = model.score(X_test,y_test)
+display(score)
 
