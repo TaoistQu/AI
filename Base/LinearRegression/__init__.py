9244db612857476a1d2b6ec78d7b7dab671cf452
TaoistQu <qulei_20180331@163.com>
2022-11-28 01:49:57 +0800

    用sklearn简单实现线性回归

---
+++ Base/LinearRegression/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2022 #
+# @Time    : 2022/11/28 1:35
+# @Author  : TaoistQu
+# @Email   : qulei_20180331@163.com
+# @File    : __init__.py.py
+# @Software: PyCharm
---
+++ Base/LinearRegression/linearRegression.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2022 #
+# @Time    : 2022/11/28 1:35
+# @Author  : TaoistQu
+# @Email   : qulei_20180331@163.com
+# @File    : linearRegression.py
+# @Software: PyCharm
+import numpy as np
+import matplotlib.pyplot as plt
+from IPython.core.display import display
+from sklearn.linear_model import LinearRegression
+
+x = np.linspace(0,10,20) +np.random.randn(20)
+y = np.linspace(0,12,20) + np.random.randn(20)
+
+plt.scatter(x,y)
+plt.show()
+
+linear = LinearRegression()
+linear.fit(x.reshape(-1,1),y)
+w = linear.coef_
+display(w)
+b = linear.intercept_
+display(b)
+
+plt.scatter(x,y)
+plt.plot(x,w*x+b,c='r')
+plt.show()
+
+x_test = np.linspace(2,8,50)
+y_ = linear.predict(x_test.reshape(-1,1))
+plt.scatter(x,y)
+plt.plot(x_test,y_,c='g')
+plt.show()
+
+
+
+
