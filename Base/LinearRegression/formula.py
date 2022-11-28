e523ce01655c287fa59919327be77024ddb07e28
TaoistQu <qulei_20180331@163.com>
2022-11-28 23:13:37 +0800

    用公式的方式实现线性回归

---
+++ Base/LinearRegression/formula.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2022 #
+# @Time    : 2022/11/28 22:51
+# @Author  : TaoistQu
+# @Email   : qulei_20180331@163.com
+# @File    : formula.py
+# @Software: PyCharm
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+x = np.linspace(0,12,20)+np.random.randn(20)
+y = np.linspace(2,12,20)+np.random.randn(20)
+ones = np.ones(shape=(20,1))
+X = np.hstack((ones,x.reshape(-1,1)))
+
+selt = np.linalg.inv(np.dot(X.T,X))@X.T@y
+print(selt)
+
+linear = LinearRegression()
+linear.fit(X,y)
+print(linear.coef_[1],linear.intercept_)
+
+
+
