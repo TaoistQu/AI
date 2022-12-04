f33366075dc969d9c50ae420c4e0134d8c7f5bce
TaoistQu <qulei_20180331@163.com>
2022-12-04 22:55:06 +0800

    梯度下降法不同参数设置可视化

---
+++ Base/gradient_descent/bgd.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2022 #
+# @Time    : 2022/12/2 1:03
+# @Author  : TaoistQu
+# @Email   : qulei_20180331@163.com
+# @File    : bgd.py
+# @Software: PyCharm
+
+import numpy as np
+import matplotlib.pyplot as plt
+X = np.random.randn(100,1)
+#生成一个斜率和截距
+W,b = np.random.randint(1,10,size=2)
+y = W*X + b + np.random.randn(100,1)
+
+X = np.c_[X,np.ones(shape=(100,1))]
+
+epoches = 20000
+
+t0,t1 = 5,1000
+
+def learning_rate_schedule(t):
+    return t0/(t+t1)
+
+#初始化w0,w1....wn
+theta = np.random.randn(2,1)
+
+loss = 0
+loss_last = loss +1
+
+for i in range(epoches):
+    '''
+    if np.abs((loss_last - loss)) < 0.0001:
+        print('执行多少次%d退出！'%(i))
+        break
+    '''
+    g = X.T.dot(X.dot(theta) - y)
+    learning_rate = learning_rate_schedule(i)
+
+    theta = theta - learning_rate *g
+    loss_last = loss
+    loss = ((X.dot(theta) - y)**2).sum
+
+print('真是的斜率和截距是：\n',W,b)
+print('BGD梯度下降结果是：\n',theta)
+
+plt.scatter(X[:,0],y)
+x = np.linspace(-5,5,100)
+y = x * theta[0,0] + theta[1,0]
+plt.plot(x,y,color = 'green')
+plt.show()
