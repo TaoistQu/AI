d21fb56c4d66ce5f83b02363e6e7919ee8a3acef
TaoistQu <qulei_20180331@163.com>
2022-12-06 00:46:14 +0800

    最大最小值归一化

---
+++ Base/Normalization/max_min.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2022 #
+# @Time    : 2022/12/6 0:37
+# @Author  : TaoistQu
+# @Email   : qulei_20180331@163.com
+# @File    : max_min.py
+# @Software: PyCharm
+import numpy as np
+
+X_1 = np.random.randint(1,10,size=10)
+X_2 = np.random.randint(100,300,size=10)
+
+X = np.c_[X_1,X_2]
+print('归一化前的数据：')
+print(X)
+X_min = X.min(axis=0)
+X_max = X.max(axis=0)
+X = (X - X_min) / (X_max - X_min)
+
+print(X)
