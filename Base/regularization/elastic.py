4d90012d17bd56298042c40bb30fb5c52da72908
TaoistQu <qulei_20180331@163.com>
2022-12-11 15:53:40 +0800

    L1 L2 正则应用，Elastic_Net

--- Base/regularization/elastic.py
+++ Base/regularization/elastic.py
@@ -1,32 +1,30 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 # Copyright (C) 2022 #
-# @Time    : 2022/12/11 13:09
+# @Time    : 2022/12/11 15:42
 # @Author  : TaoistQu
 # @Email   : qulei_20180331@163.com
-# @File    : ridge.py
+# @File    : elastic.py
 # @Software: PyCharm
+
 import numpy as np
-from sklearn.linear_model import Ridge
+from sklearn.linear_model import ElasticNet
 from sklearn.linear_model import SGDRegressor
 
-X = 2*np.random.rand(100,5)
-w = np.random.randint(1,10,size=(5,1))
+X = 2*np.random.randn(100,20)
+w = np.random.randn(20,1)
 b = np.random.randint(1,10,size=1)
 y = X.dot(w) + b + np.random.randn(100,1)
 
 print('原始方程的斜率：',w.ravel())
 print('原始方程的截距：',b)
 
-ridge = Ridge(alpha=2)
-ridge.fit(X,y)
-print('岭回归求解的斜率：',ridge.coef_)
-print('岭回归求解的截距：',ridge.intercept_)
+model = ElasticNet(alpha= 1,l1_ratio=0.7)
+model.fit(X,y)
+print('弹性网络回归求解的斜率：',model.coef_)
+print('弹性网络回归求解的截距：',model.intercept_)
 
-#线性回归梯度下降法
-sgd = SGDRegressor(penalty='l2',alpha=0.001,l1_ratio=0)
+sgd = SGDRegressor(penalty='l2',alpha=0,l1_ratio=0)
 sgd.fit(X,y.reshape(-1,))
 print('随机梯度下降求解的斜率是：',sgd.coef_)
 print('随机梯度下降求解的截距是：',sgd.intercept_)
-
-
