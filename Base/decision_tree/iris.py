bab9216000e83f002654bde6ef4641b3f702f493
TaoistQu <qulei_20180331@163.com>
2022-12-24 21:08:30 +0800

    使用iris数据集计算决策树

--- Base/decision_tree/iris.py
+++ Base/decision_tree/iris.py
@@ -1,25 +1,44 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 # Copyright (C) 2022 #
-# @Time    : 2022/12/22 20:39
+# @Time    : 2022/12/24 20:44
 # @Author  : TaoistQu
 # @Email   : qulei_20180331@163.com
 # @File    : iris.py
 # @Software: PyCharm
 # description:
 
-from sklearn.datasets import load_iris
+import numpy as np
 from sklearn.tree import DecisionTreeClassifier
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
 from sklearn import tree
 import graphviz
 import os
 
-iris = load_iris()
-clf = DecisionTreeClassifier()
-clf_iris = clf.fit(iris.data, iris.target)
+iris = datasets.load_iris()
+X = iris['data']
+y = iris['target']
+name = iris['target_names']
 
-# 将决策树的结构数据导出
-path  = os.path.abspath('../image')
-dot_data = tree.export_graphviz(clf, out_file =os.path.join(path,'.\iris') )
-graph = graphviz.Source.from_file(os.path.join(path,'.\iris'))
-graph.render(os.path.join(path,'.\iris'),format='pdf')# 保存到文件中
+X_train,X_test,y_train,y_test = train_test_split(X,y,random_state=256)
+model = DecisionTreeClassifier(max_depth=None,criterion='entropy')
+model.fit(X_train,y_train)
+y_ = model.predict(X_test)
+
+print('真实类别是：',y_test)
+print('算法预测是：',y_)
+print('准确率是：',model.score(X_test,y_test))
+path = os.path.abspath('../image')
+dot_data = tree.export_graphviz(model,feature_names=iris['feature_names'],
+                                out_file=os.path.join(path,'./iris'),
+                                class_names=name,
+                                filled=True,
+                                rounded=True,
+                                fontname='kaiTi',)
+
+graph = graphviz.Source.from_file(os.path.join(path,'./iris'))
+graph.render(os.path.join(path,'./iris'),format='png')
+
+
+
---
+++ Base/decision_tree/my_graphviz.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2022 #
+# @Time    : 2022/12/22 20:39
+# @Author  : TaoistQu
+# @Email   : qulei_20180331@163.com
+# @File    : my_graphviz.py
+# @Software: PyCharm
+# description:
+
+from sklearn.datasets import load_iris
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
+import graphviz
+import os
+
+iris = load_iris()
+clf = DecisionTreeClassifier()
+clf_iris = clf.fit(iris.data, iris.target)
+
+# 将决策树的结构数据导出
+path  = os.path.abspath('../image')
+dot_data = tree.export_graphviz(clf, out_file =os.path.join(path,'.\iris') )
+graph = graphviz.Source.from_file(os.path.join(path,'.\iris'))
+graph.render(os.path.join(path,'.\iris'),format='pdf')# 保存到文件中
