186a8846c86206cad3b558dceb79fb8a12e94fb3
TaoistQu <qulei_20180331@163.com>
2022-12-25 18:15:57 +0800

    图像加减乘除运算

---
+++ opencv/operation/add.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2022 #
+# @Time    : 2022/12/25 17:46
+# @Author  : TaoistQu
+# @Email   : qulei_20180331@163.com
+# @File    : add.py
+# @Software: PyCharm
+# description:
+import os
+import cv2
+
+path = os.path.abspath('../images')
+
+cat = cv2.imread(os.path.join(path,'./cat.jpeg'))
+dog = cv2.imread(os.path.join(path,'./dog.jpeg'))
+cat = cat[:360,:499]
+
+#new_img = cv2.add(cat,dog)
+#new_img = cv2.multiply(cat,dog)
+#new_img = cv2.subtract(cat,dog)
+new_img = cv2.divide(cat,dog)
+
+cv2.imshow('dog',dog)
+cv2.imshow('cat',cat)
+
+cv2.imshow('new_img',new_img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
