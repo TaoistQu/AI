e52fe7693f96d2de170a33139ed3ee67de808694
TaoistQu <qulei_20180331@163.com>
2023-01-05 23:24:40 +0800

    形态学图片二值化

---
+++ opencv/morphology/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2022 #
+# @Time    : 2023/1/5 22:51
+# @Author  : TaoistQu
+# @Email   : qulei_20180331@163.com
+# @File    : __init__.py.py
+# @Software: PyCharm
+# description:图像形态学
---
+++ opencv/morphology/threshold.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2022 #
+# @Time    : 2023/1/5 23:12
+# @Author  : TaoistQu
+# @Email   : qulei_20180331@163.com
+# @File    : threshold.py
+# @Software: PyCharm
+# description:
+import numpy as np
+import os
+import cv2
+
+path = os.path.abspath('../images')
+img = cv2.imread(os.path.join(path, 'dog.jpeg'))
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+#返回两个结果，一个阈值，另一个处理后的图片
+ret, dst = cv2.threshold(gray, 80,255,cv2.THRESH_BINARY)
+cv2.imshow('dog', np.hstack((gray, dst)))
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
