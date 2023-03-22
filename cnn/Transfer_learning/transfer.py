b9ae0cc0780e81bb55f72c15a5a78f452e09d36d
TaoistQu <13110402600@163.com>
2023-03-22 22:23:13 +0800

    qiayi xuexi xunlain reset_net网络

--- cnn/Transfer_learning/transfer.py
+++ cnn/Transfer_learning/transfer.py
@@ -1,23 +1,39 @@
 # !/usr/bin/env python3.8
 # -*- coding:utf-8 -*-
-# @FileName  :train.py
-# @Time      :2023/3/21 下午10:05
+# @FileName  :transfer.py
+# @Time      :2023/3/22 下午8:59
 # @Author    :TaoistQu
 # Email      :qulei_20180331@163.com
-# description:
+# description:使用迁移学习训练网络
 
+from keras import Sequential
+from keras.applications.resnet import ResNet50
+from keras.layers import Dense
+import keras
 from keras.preprocessing.image import ImageDataGenerator
-from model import resnet34, resnet50
 
+resnet50 = ResNet50(include_top=False, classes=10, pooling='avg')
+model = Sequential()
+model.add(resnet50)
+model.add(Dense(10, activation='softmax'))
+
+model.summary()
+
+#固定某层不训练
+
+model.layers[0].trainable = False
+
+model.summary()
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
 
 train_dir  = '../data/training/training'
 valid_dir = '../data/validation/validation'
 label_file ='../data/monkey_labels.txt'
 
 def main():
-    # 生成数据
+    # 生成数据，使用迁移学习时对数据预处理，要对数据预处理采用相同的方式
     train_datagen = ImageDataGenerator(
-        rescale=1.0 / 255.0,
+        preprocessing_function= keras.applications.resnet.preprocess_input,
         rotation_range=40,
         width_shift_range=0.2,
         height_shift_range=0.2,
@@ -27,6 +43,7 @@ def main():
         vertical_flip=True,
         fill_mode='nearest'
     )
+
 
     # 从目录中读取图片
     height = 224
@@ -44,7 +61,8 @@ def main():
                                                         class_mode='categorical')
 
     valid_datagen = ImageDataGenerator(
-        rescale=1.0 / 255.0)
+        preprocessing_function=keras.applications.resnet.preprocess_input
+    )
 
     valid_generator = valid_datagen.flow_from_directory(valid_dir, target_size=(
         height, width),
@@ -52,20 +70,19 @@ def main():
                                                         shuffle=True,
                                                         class_mode='categorical')
 
-    model34 = resnet34(num_classes=10)
-    model34.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc'])
+
     train_num = train_generator.samples
     valid_num = valid_generator.samples
 
-    history = model34.fit(train_generator,
+    history = model.fit(train_generator,
                           steps_per_epoch=train_num // batch_size,
-                          epochs=40,
+                          epochs=30,
                           validation_data=valid_generator,
                           validation_steps=valid_num // batch_size)
 
 
+    model.save('./ResNet50.h5')
+
+
 if __name__ == '__main__':
     main()
-
-
-
